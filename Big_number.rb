f870496403552dd03372f8cae92835af8f0f37b1
Qinrui Wang <qinruiw@student.unimelb.edu.au>
2015-08-08 17:52:26 +1000

    practice of handling big number in Ruby added

---
+++ Big_number.rb
@@ -0,0 +1,18 @@
+# This is a example program exams the accuracy of integer
+
+# Introduction
+puts "None like C, Integers in ruby should be able to handle numbers very large"
+
+# Main functions
+# Gets inputs
+puts "Please enter the first number, this number could be very big"
+first_num = gets.to_i
+puts "Please enter the second number, this number could be very big"
+second_num = gets.to_i
+
+# Give outputs
+puts "The first number you entered was: " + first_num.to_s
+puts "The second number you entered was: " + second_num.to_s
+
+# Ending
+puts "The big numbers should be stored and exported correctly"
